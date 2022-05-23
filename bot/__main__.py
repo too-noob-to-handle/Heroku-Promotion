9f5e390d897a2420856cf4ff5428b0592d2d5922
too-noob-to-handle <98410308+too-noob-to-handle@users.noreply.github.com>
2022-05-23 03:14:32 -0400

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -33,19 +33,19 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>╭──《🌐 Bᴏᴛ Sᴛᴀᴛɪsᴛɪᴄs 🌐》</b>\n' \
+    stats = f'<b>╭──《🌐 Sᴛᴀᴛɪsᴛɪᴄs 》</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>├  ▶ Rᴜɴɴɪɴɢ Sɪɴᴄᴇ ▶ : {currentTime}</b>\n' \
-            f'<b>├  💾 Tᴏᴛᴀʟ Dɪsᴋ Sᴘᴀᴄᴇ : {total}</b>\n' \
-            f'<b>├  📀 Tᴏᴛᴀʟ Usᴇᴅ Sᴘᴀᴄᴇ : {used}</b>\n' \
-            f'<b>├  💿 Tᴏᴛᴀʟ Fʀᴇᴇ Sᴘᴀᴄᴇ : {free}</b>\n' \
-            f'<b>├  🔼 Tᴏᴛᴀʟ Uᴘʟᴏᴀᴅ : {sent}</b>\n' \
-            f'<b>├  🔽 Tᴏᴛᴀʟ Dᴏᴡɴʟᴏᴀᴅ : {recv}</b>\n' \
-            f'<b>├  🖥️ Cᴘᴜ : {cpuUsage}%</b>\n' \
-            f'<b>├  🎮 Rᴀᴍ : {memory}%</b>\n' \
-            f'<b>├  💽 Dɪsᴋ : {disk}%</b>\n' \
+            f'<b>├   Rᴜɴɴɪɴɢ Sɪɴᴄᴇ  : {currentTime}</b>\n' \
+            f'<b>├   Tᴏᴛᴀʟ Dɪsᴋ Sᴘᴀᴄᴇ : {total}</b>\n' \
+            f'<b>├   Tᴏᴛᴀʟ Usᴇᴅ Sᴘᴀᴄᴇ : {used}</b>\n' \
+            f'<b>├   Tᴏᴛᴀʟ Fʀᴇᴇ Sᴘᴀᴄᴇ : {free}</b>\n' \
+            f'<b>├   Tᴏᴛᴀʟ Uᴘʟᴏᴀᴅ : {sent}</b>\n' \
+            f'<b>├   Tᴏᴛᴀʟ Dᴏᴡɴʟᴏᴀᴅ : {recv}</b>\n' \
+            f'<b>├   Cᴘᴜ : {cpuUsage}%</b>\n' \
+            f'<b>├   Rᴀᴍ : {memory}%</b>\n' \
+            f'<b>├   Dɪsᴋ : {disk}%</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>╰──《 ☣️ @SparkXcloud ☣️ 》</b>'
+            f'<b>╰──《 BDH 》</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
 
@@ -65,7 +65,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         else :
             sendMarkup(start_string, context.bot, update, reply_markup)
     else :
-        sendMarkup(f"Oops! not an Authorized user.\nPlease deploy your own <b>SparkXcloud-Gdrive-MirrorBot</b>.", context.bot, update, reply_markup)
+        sendMarkup(f"Oops! not an Authorized user.\nPlease deploy your own", context.bot, update, reply_markup)
 
 
 def restart(update, context):
